54a44ca5f056439331ebf5acde997b921943a203
EgorIfr <asd141299@gmail.com>
2025-11-12 18:40:01 +0300

    Initial commit: Tic-tac-toe game

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    function Square({ value }) {
+        function handleClick() {
+            console.log('Клик!')
+        }
+
+        return <button className="square" onClick={handleClick}>{ value }</button>
+    }
+
+
+    function Board() {
+        return (
+            <div>
+                <div className="board-row">
+                    <Square value={1} />
+                    <Square value={2} />
+                    <Square value={3} />
+                </div>
+                <div className="board-row">
+                    <Square value={4} />
+                    <Square value={5} />
+                    <Square value={6} />
+                </div>
+                <div className="board-row">
+                    <Square value={7} />
+                    <Square value={8} />
+                    <Square value={9} />
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <>
+            <Board />
+        </>
+    )
+}
+
+export default App
