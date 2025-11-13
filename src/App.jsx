b47c075ed0b460b80b8a66c96bff0fe86045e3c6
EgorIfr <asd141299@gmail.com>
2025-11-13 14:27:01 +0300

    Добавил переменую состояния value

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-    function Square({ value }) {
+    function Square() {
         const [value, setValue] = useState(null);
 
         function handleClick() {
-            console.log('Клик!')
+            setValue('X');
         }
 
         return <button className="square" onClick={handleClick}>{ value }</button>
